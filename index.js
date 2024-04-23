565ff4e36b69f94da6a0924aa9261bd3c1289751
xiomee <xiome@gmail.com>
2024-04-23 07:42:42 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const notesList = 
+[
+ {
+    date: "21/11/2023",
+    title: "Mid text exam",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, pariatur quaerat fuga minima nihil, molestiae unde quibusdam quo magni cupiditate consequatur quis optio architecto distinctio explicabo aut vitae similique. Temporibus!"
+ },
+ {
+    date:"21/11/2023",
+    title: "Mid text exam",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, pariatur quaerat fuga minima nihil, molestiae unde quibusdam quo magni cupiditate consequatur quis optio architecto distinctio explicabo aut vitae similique. Temporibus!"
+ },
+ {
+    date:"21/11/2023",
+    title: "Mid text exam",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, pariatur quaerat fuga minima nihil, molestiae unde quibusdam quo magni cupiditate consequatur quis optio architecto distinctio explicabo aut vitae similique. Temporibus!"
+ },
+]
